4efeb7ee1f3b87d0748a9452dfd5fc262d814dff
Brian Hechinger <wonko@4amlunch.net>
2023-12-14 22:16:21 +0000

    Added some proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
 #[tokio::main]
 async fn main() {
     let ping_result = nitrapi::ping::all().await;
-
-    if let Ok(ping) = ping_result {
-        println!("ping\n\t{}: {}", ping.status, ping.message)
-    };
+    match ping_result {
+        Ok(ping) => println!("ping\n\t{}: {}", ping.status, ping.message),
+        Err(err) => println!("error pinging: {}", err)
+    }
 
     let maintenance_result = nitrapi::maintenance::all().await;
-
-    if let Ok(maintenance) = maintenance_result {
-        println!("maintenance\n\tstatus: {}\n\tcloud_backend: {}\n\tdomain_backend: {}\n\tdns_backend: {}\n\tpmacct_backend: {}",
-                 maintenance.status,
-                 maintenance.data.maintenance.cloud_backend,
-                 maintenance.data.maintenance.domain_backend,
-                 maintenance.data.maintenance.dns_backend,
-                 maintenance.data.maintenance.pmacct_backend
-        )
-    };
+    match maintenance_result {
+        Ok(m) =>
+            println!("maintenance\n\tstatus: {}\n\tcloud_backend: {}\n\tdomain_backend: {}\n\tdns_backend: {}\n\tpmacct_backend: {}",
+                     m.status,
+                     m.data.maintenance.cloud_backend,
+                     m.data.maintenance.domain_backend,
+                     m.data.maintenance.dns_backend,
+                     m.data.maintenance.pmacct_backend
+            ),
+        Err(err) => println!("error getting maintenance status: {}", err)
+    }
 
     let version_result = nitrapi::version::all().await;
-
-    if let Ok(ping) = version_result {
-        println!("ping\n\t{}: {}", ping.status, ping.message)
-    };
+    match version_result {
+        Ok(version) => println!("version\n\t{}: {}", version.status, version.message),
+        Err(err) => println!("error pinging: {}", err)
+    }
 }
