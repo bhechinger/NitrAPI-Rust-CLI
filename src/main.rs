c41ae5800699a88826c9b3f7754149654b46ac8d
Brian Hechinger <wonko@4amlunch.net>
2023-12-14 22:11:28 +0000

    Added all the endpoints that don't require auth

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 #[tokio::main]
 async fn main() {
-    let result = nitrapi::ping::all().await;
+    let ping_result = nitrapi::ping::all().await;
 
-    if let Ok(ping) = result{
-        println!("{}", ping)
+    if let Ok(ping) = ping_result {
+        println!("ping\n\t{}: {}", ping.status, ping.message)
+    };
+
+    let maintenance_result = nitrapi::maintenance::all().await;
+
+    if let Ok(maintenance) = maintenance_result {
+        println!("maintenance\n\tstatus: {}\n\tcloud_backend: {}\n\tdomain_backend: {}\n\tdns_backend: {}\n\tpmacct_backend: {}",
+                 maintenance.status,
+                 maintenance.data.maintenance.cloud_backend,
+                 maintenance.data.maintenance.domain_backend,
+                 maintenance.data.maintenance.dns_backend,
+                 maintenance.data.maintenance.pmacct_backend
+        )
+    };
+
+    let version_result = nitrapi::version::all().await;
+
+    if let Ok(ping) = version_result {
+        println!("ping\n\t{}: {}", ping.status, ping.message)
     };
 }
