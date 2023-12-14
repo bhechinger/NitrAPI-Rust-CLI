d3522058bf23e08ec248cbcc3ddeb9cef90d5996
Brian Hechinger <wonko@4amlunch.net>
2023-12-14 21:38:53 +0000

    Added flakes, direnv, updated gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let result = nitrapi::ping::all().await;
+
+    if let Ok(ping) = result{
+        println!("{}", ping)
+    };
 }
